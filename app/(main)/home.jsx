c7a682b45677e04712eeb0455156947a1c8b3629
Kira Leugard <kirakazama01@gmail.com>
2024-07-30 14:08:12 +0700

    Update

---
+++ app/(main)/home.jsx
@@ -0,0 +1,29 @@
+import { Alert, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import Button from "../../components/Button";
+import { useAuth } from "../../context/AuthContext";
+import { supabase } from "../../lib/supabase";
+
+const home = () => {
+  const { setAuth } = useAuth();
+
+  const onLogout = async () => {
+    setAuth(null);
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      Alert.alert("Sign out", "Error signing out!");
+    }
+  };
+
+  return (
+    <ScreenWrapper>
+      <Text>home</Text>
+      <Button title="Logout" onPress={onLogout} />
+    </ScreenWrapper>
+  );
+};
+
+export default home;
+
+const styles = StyleSheet.create({});
